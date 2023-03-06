74f6731d2b4aa73e0ac9600018af7196d22c4059
boobalan08 <boobalans42@gmail.com>
2023-03-06 10:07:58 +0530

    init

---
+++ src/data/data.js
@@ -0,0 +1,30 @@
+import invento from "../assets/projects/Invento.png";
+import expense from "../assets/projects/Moneymanager.png";
+import movie from "../assets/projects/movie.png";
+
+export const data = [
+  {
+    id: 1,
+    name: "Inventory Billing App(MERN)",
+    image: invento,
+    github: "https://github.com/boobalan08/inventory-billing-app-frontend",
+    backend: "https://github.com/boobalan08/inventory-billing-app-backend",
+    live: "https://musical-muffin-35b96a.netlify.app/",
+  },
+  {
+    id: 2,
+    name: "Money Manager App(MERN)",
+    image: expense,
+    github: "https://github.com/boobalan08/money-manager-frontend",
+    backend: "https://github.com/boobalan08/money-manager-backend",
+    live: "https://vocal-truffle-1699cb.netlify.app/login",
+  },
+  {
+    id: 3,
+    name: "Movie App(MERN)",
+    image: movie,
+    github: "https://github.com/boobalan08/React-Movies_App",
+    backend: "https://github.com/boobalan08/express",
+    live: "https://movie-app-reacts.netlify.app/",
+  },
+];
